dbb36d3365eeda7cac9d5d3f779dcbe089b61c31
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-08-18 18:03:06 +0530

    Solved letter combination problem.

--- Medium/17LetterCombinationsofaPhoneNumber.py
+++ Medium/17LetterCombinationsofaPhoneNumber.py
@@ -1,27 +1,28 @@
-COMBINATIONS = {
-    '2':('a','b','c'),
-    '3':('d','e','f'),
-    '4':('g','h','i'),
-    '5':('j','k','l'),
-    '6':('m','n','o'),
-    '7':('p','q', 'r','s'),
-    '8':('t','u','v'),
-    '9':('w','x','y', 'z'),
-}
 
 class Solution(object):
     def letterCombinations(self, digits):
+        COMBINATIONS = {
+            '2':('a','b','c'),
+            '3':('d','e','f'),
+            '4':('g','h','i'),
+            '5':('j','k','l'),
+            '6':('m','n','o'),
+            '7':('p','q', 'r','s'),
+            '8':('t','u','v'),
+            '9':('w','x','y', 'z'),
+        }
         if len(digits) < 1:
             return ""
         if len(digits) == 1:
             return COMBINATIONS[digits[0]]
-        res = []
-        for i in range(len(digits)-1):
-            for letter1 in COMBINATIONS[digits[i]]:
-                for letter2 in COMBINATIONS[digits[i+1]]:
-                    word = letter1 + letter2
-                    res.append(word)
+        res = [""]
+        for digit in digits:
+            new = []
+            for i in res:
+                for item in COMBINATIONS[digit]:
+                    new.append(i+item)
+            res = new
         return res
-
+    
 a = Solution()
-print(a.letterCombinations("2"))
+print(a.letterCombinations("223"))
